cc1364b0886ad84dd74bfa602b3efffa6264be12
catiaribeiro <catia540@hotmail.com>
2023-12-10 11:24:58 +0000

    day one complete!

--- advent-of-code-2023-csharp/Program.cs
+++ advent-of-code-2023-csharp/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using advent_of_code_2023_csharp.day1;
+
 Console.WriteLine("Hello, World!");
+
+dayone dayone = new dayone();
+dayone.run();
---
+++ advent-of-code-2023-csharp/day1/dayone.cs
@@ -0,0 +1,39 @@
+namespace advent_of_code_2023_csharp.day1;
+
+public class dayone
+{
+    public void run()
+    {
+        Console.WriteLine("Day 1!");
+
+        string fileloc =
+            "/Users/catiaribeiro/RiderProjects/advent-of-code-2023-csharp/advent-of-code-2023-csharp/day1/input.txt";
+
+        int total = 0;
+
+        using (StreamReader read = new StreamReader(fileloc))
+        {
+            string line;
+            while ((line = read.ReadLine()) != null)
+            {
+                char[] numbers = Array.Empty<char>();
+                foreach (char c in line)
+                {
+                    if (char.IsNumber(c))
+                    {
+                        numbers = numbers.Concat(new char[] { c }).ToArray();
+                    }
+                }
+
+                if (numbers.Length == 0)
+                {
+                    continue;
+                }
+                
+                total += int.Parse(numbers[0] + numbers[^1].ToString());
+            }
+
+            Console.WriteLine("Answer: " + total);
+        }
+    }
+}
